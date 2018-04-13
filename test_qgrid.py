2b8035f30d6a6a45cb34a6754dc21e1ff558636c
felixt <felix.therrien@gmail.com>
2018-04-12 18:01:21 -0600

    test_qgrid.py has fulfilled it's goal

--- test_qgrid.py
+++ test_qgrid.py
@@ -6,6 +6,8 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+tol = 1e-12
+
 # Primittive structure
 A = Structure([[0.5, 0.5, 0],[0.5, 0, 0.5],[0, 0.5, 0.5]])
 A.add_atom(0,0,0,'Si')
@@ -35,16 +37,21 @@ for i in range(-corner[0,0], corner[0,0]):
     for j in range(-corner[0,1], corner[0,1]):
         for k in range (-corner[0,2], corner[0,2]):
             p = np.array([i,j,k]).dot(rsc)
-            if (p.dot(irpc) <= 0.5).all() and (p.dot(irpc) >= -0.5).all():
+            if (p.dot(irpc) >= -0.5).all() and (p.dot(irpc) < 0.5 - tol).all():
                 list_in.append(p.tolist())
 
 list_in = np.array(list_in)
-            
+
+# Uniform grid
+
+x = np.linspace(-0.5,0.5,7)[:-1]
+
+qgrid = np.array(np.meshgrid(x,x,x)).reshape(3,-1,order = 'F').T.dot(rpc)
 
 # Easy PC
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(list_in[:,0], list_in[:,1], list_in[:,2])
+ax.scatter(list_in[:,0], list_in[:,1], list_in[:,2])
 ax.quiver(np.zeros(3), np.zeros(3), np.zeros(3), rpc[:,0], rpc[:,1], rpc[:,2])
 ax.quiver(np.zeros(3), np.zeros(3), np.zeros(3), rsc[:,0], rsc[:,1], rsc[:,2], color ='red')
 
@@ -67,6 +74,11 @@ qpoints = np.array([
         [0.000000000,   0.335187099,  -1.005561297]])*2*np.pi
 
 ax.scatter(qpoints[:,0], qpoints[:,1], qpoints[:,2], color = 'green')
+ax.scatter(qgrid[:,0], qgrid[:,1], qgrid[:,2], color = 'orange')
 
-ax.view_init(30,30)
-plt.savefig('In_PC.png')
+ax.view_init(40,-45)
+plt.savefig('In_PC_6x6_40.45.png')
+
+print np.shape(list_in)
+
+plt.show()
